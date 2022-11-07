118d70f84c0517624932e007203a07b0f35941f1
Belteshazzar Luwalekeah <daniel.cooke900@gmail.com>
2022-11-07 10:17:58 -0700

    Update zenas_streamlit.py
    added a stop to debug

--- zenas_streamlit.py
+++ zenas_streamlit.py
@@ -7,8 +7,10 @@ from urllib.error import URLError
 import streamlit
 import snowflake.connector
 import pandas
+
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
 # connect to snowflake
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 # run a snowflake query and put it all in a var called my_catalog
